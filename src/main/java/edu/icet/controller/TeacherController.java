ac97ee2ba5573d24b9be694e7f3d7109d59a78e2
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-23 19:37:23 +0530

    feat:  TeacherService constructor inject

--- src/main/java/edu/icet/controller/TeacherController.java
+++ src/main/java/edu/icet/controller/TeacherController.java
@@ -1,11 +1,16 @@
 package edu.icet.controller;
 
+import edu.icet.service.TeacherService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/teacher")
 @Tag(name = "Teacher-Controller",description = "Teacher API Collection")
 public class TeacherController {
+    private final TeacherService service;
+
 }
