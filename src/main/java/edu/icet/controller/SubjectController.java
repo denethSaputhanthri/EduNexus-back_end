d34f57fece986460f4032492beb6016a57b212b4
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-27 23:43:12 +0530

    feat: SubjectService constructor inject

--- src/main/java/edu/icet/controller/SubjectController.java
+++ src/main/java/edu/icet/controller/SubjectController.java
@@ -1,11 +1,16 @@
 package edu.icet.controller;
 
+import edu.icet.service.SubjectService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/subject")
 @Tag(name = "Subject-Controller",description = "Subject API Collection")
 public class SubjectController {
+    final SubjectService service;
+
 }
