a3f05f14e24aa2afe3769f05634a37baa6582303
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-29 00:27:10 +0530

    feat: create user controller layer add

---
+++ src/main/java/edu/icet/controller/UserController.java
@@ -0,0 +1,11 @@
+package edu.icet.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@Tag(name = "User-Controller",description = "User API collection...")
+public class UserController {
+}
