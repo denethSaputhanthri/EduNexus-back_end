7ef5b02be0aad7042148badb24caff8dc742d54c
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-29 00:37:20 +0530

    feat: UserService constructor inject

--- src/main/java/edu/icet/controller/UserController.java
+++ src/main/java/edu/icet/controller/UserController.java
@@ -1,11 +1,17 @@
 package edu.icet.controller;
 
+import edu.icet.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 @Tag(name = "User-Controller",description = "User API collection...")
 public class UserController {
+
+    final UserService service;
+
 }
