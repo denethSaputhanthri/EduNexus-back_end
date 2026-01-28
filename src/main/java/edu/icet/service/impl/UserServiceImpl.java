477882ad58ef545bae80d0daca7d7bb6511a88d3
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-29 00:30:15 +0530

    feat: UserRepository and Model mapper constructor inject

--- src/main/java/edu/icet/service/impl/UserServiceImpl.java
+++ src/main/java/edu/icet/service/impl/UserServiceImpl.java
@@ -1,7 +1,15 @@
 package edu.icet.service.impl;
 
+import edu.icet.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl {
+
+    final UserRepository repository;
+    final ModelMapper mapper;
+
 }
