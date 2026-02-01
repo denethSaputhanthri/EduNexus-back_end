1b78979efea91638ccc61b94c3718f1137f733d8
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-01 13:01:12 +0530

    feat: add Classes entity and DTO

---
+++ src/main/java/edu/icet/model/dto/ClassesDTO.java
@@ -0,0 +1,20 @@
+package edu.icet.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class ClassesDTO {
+    private Integer classes;
+    private Integer schoolId;
+    private Integer academicYearId;
+    private String className;
+    private String section;
+    private String classTeacherId;
+}
