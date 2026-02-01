38edc84652ed485b610c64648e947f4ddb8487d2
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-01 13:27:55 +0530

    feat: ClassesService,ClassesRepository and Model mapper constructor inject

--- src/main/java/edu/icet/controller/ClassesController.java
+++ src/main/java/edu/icet/controller/ClassesController.java
@@ -1,11 +1,17 @@
 package edu.icet.controller;
 
+import edu.icet.service.ClassesService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/classes")
 @Tag(name = "Classes-Controller",description = "Classes API Collection...")
 public class ClassesController {
+
+    final ClassesService service;
+
 }
--- src/main/java/edu/icet/service/impl/ClassesServiceImpl.java
+++ src/main/java/edu/icet/service/impl/ClassesServiceImpl.java
@@ -1,12 +1,21 @@
 package edu.icet.service.impl;
 
 import edu.icet.model.dto.ClassesDTO;
+import edu.icet.repository.ClassesRepository;
 import edu.icet.service.ClassesService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@RequiredArgsConstructor
+
 public class ClassesServiceImpl implements ClassesService {
+
+    final ClassesRepository repository;
+    final ModelMapper mapper;
+
     @Override
     public void addClasses(ClassesDTO classes) {
 
