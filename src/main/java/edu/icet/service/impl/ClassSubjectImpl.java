de0d97c7cfda4ab505d2abb83091ad1a8a54674a
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-04 16:22:52 +0530

    feat: create service layer and controller layer

---
+++ src/main/java/edu/icet/service/impl/ClassSubjectImpl.java
@@ -0,0 +1,31 @@
+package edu.icet.service.impl;
+
+import edu.icet.model.dto.ClassSubjectDTO;
+import edu.icet.service.ClassSubjectService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClassSubjectImpl implements ClassSubjectService {
+
+    @Override
+    public void addClassSubject(ClassSubjectDTO classSubject) {
+
+    }
+
+    @Override
+    public void updateClassSubject(ClassSubjectDTO classSubject) {
+
+    }
+
+    @Override
+    public void deleteClassSubject(Integer classSubjectId) {
+
+    }
+
+    @Override
+    public List<ClassSubjectDTO> getAll() {
+        return List.of();
+    }
+}
