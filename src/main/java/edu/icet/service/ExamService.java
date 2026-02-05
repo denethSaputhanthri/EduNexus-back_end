61ed7364e28695e7c80209a9999120fa349ed3e0
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-05 23:25:48 +0530

    feat: add ExamService interface, implement AcademicYearService methods

---
+++ src/main/java/edu/icet/service/ExamService.java
@@ -0,0 +1,18 @@
+package edu.icet.service;
+
+import edu.icet.Enum.ExamType;
+import edu.icet.model.dto.ExamDTO;
+
+import java.util.List;
+
+public interface ExamService {
+    void addExam(ExamDTO exam);
+    void updateExam(ExamDTO exam);
+    void deleteExam(Integer examId);
+    List<ExamDTO>getAll();
+    ExamDTO searchById(Integer examId);
+    List<ExamDTO> searchByAcademicYearId(Integer academicYearId);
+    List<ExamDTO> searchByTermId(Integer termId);
+    List<ExamDTO> searchByType(ExamType type);
+    List<ExamDTO> searchByName(String examName);
+}
