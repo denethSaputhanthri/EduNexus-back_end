113d72169a3250f0f4dc62d951e42c2df08bdf60
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-07 20:49:02 +0530

    feat: create service layer and controller layer

---
+++ src/main/java/edu/icet/service/impl/TimeSlotServiceImpl.java
@@ -0,0 +1,41 @@
+package edu.icet.service.impl;
+
+import edu.icet.Enum.DayOfWeek;
+import edu.icet.model.dto.TimeTableDTO;
+import edu.icet.service.TimeTableService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class TimeSlotServiceImpl implements TimeTableService {
+
+    @Override
+    public void addAll(TimeTableDTO timeTable) {
+
+    }
+
+    @Override
+    public void updateAll(TimeTableDTO timeTable) {
+
+    }
+
+    @Override
+    public void deleteAll(Integer timeTableId) {
+
+    }
+
+    @Override
+    public List<TimeTableDTO> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public TimeTableDTO searchById(Integer timeTableId) {
+        return null;
+    }
+
+    @Override
+    public List<TimeTableDTO> searchByDayTimeTable(DayOfWeek day) {
+        return List.of();
+    }
+}
