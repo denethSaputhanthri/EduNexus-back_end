f7c28e7220d1865550102efbb2cd571e85174f31
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-08 11:13:37 +0530

    feat: Service,Repository and Model mapper constructor inject

--- src/main/java/edu/icet/controller/MarkController.java
+++ src/main/java/edu/icet/controller/MarkController.java
@@ -1,11 +1,17 @@
 package edu.icet.controller;
 
+import edu.icet.service.MarkService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/mark")
 @Tag(name = "Mark-Controller",description = "Mark API Collection...")
 public class MarkController {
+
+    final private MarkService service;
+
 }
--- src/main/java/edu/icet/service/impl/MarkServiceImpl.java
+++ src/main/java/edu/icet/service/impl/MarkServiceImpl.java
@@ -1,13 +1,20 @@
 package edu.icet.service.impl;
 
 import edu.icet.model.dto.MarkDTO;
+import edu.icet.repository.MarkRepository;
 import edu.icet.service.MarkService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MarkServiceImpl implements MarkService {
+
+    final private MarkRepository repository;
+    final private ModelMapper mapper;
 
     @Override
     public void addMark(MarkDTO mark) {
