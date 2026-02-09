653abb87924a477b9b524c7e7c48d012949883d8
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-09 17:37:48 +0530

    feat: add missing annotation

--- src/main/java/edu/icet/model/entity/AttendanceEntity.java
+++ src/main/java/edu/icet/model/entity/AttendanceEntity.java
@@ -3,11 +3,13 @@ package edu.icet.model.entity;
 import edu.icet.utill.AStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
