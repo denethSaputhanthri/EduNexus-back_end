08a35321a358ea990fe65ac262c28a18e5dab9f7
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-09 17:06:46 +0530

    feat: add AttendanceService interface, implement AttendanceService methods

---
+++ src/main/java/edu/icet/service/AttendanceService.java
@@ -0,0 +1,20 @@
+package edu.icet.service;
+
+import edu.icet.model.dto.AttendanceDTO;
+import edu.icet.utill.AStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+public interface AttendanceService {
+    void addAttendance(AttendanceDTO attendance);
+    void updateAttendance(AttendanceDTO attendance);
+    void deleteAttendance(Integer attendanceId);
+    List<AttendanceDTO>getAll();
+    AttendanceDTO searchByAttendanceId(Integer attendanceId);
+    AttendanceDTO searchByStudentId(Integer studentId);
+    List<AttendanceDTO> searchByClassId(Integer classId);
+    List<AttendanceDTO> searchByStatus(AStatus status);
+    List<AttendanceDTO> searchByDate(LocalDate date);
+}
