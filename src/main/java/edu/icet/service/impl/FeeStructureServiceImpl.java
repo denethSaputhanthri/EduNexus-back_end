2dced1cc2783a5d5724b58477afbec42d1dcbddf
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-10 23:58:32 +0530

    feat: create service layer and controller layer

---
+++ src/main/java/edu/icet/service/impl/FeeStructureServiceImpl.java
@@ -0,0 +1,30 @@
+package edu.icet.service.impl;
+
+import edu.icet.model.dto.FeeStructureDTO;
+import edu.icet.service.FeeStructureService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FeeStructureServiceImpl implements FeeStructureService {
+    @Override
+    public void addFeeStructure(FeeStructureDTO feeStructure) {
+
+    }
+
+    @Override
+    public void updateFeeStructure(FeeStructureDTO feeStructure) {
+
+    }
+
+    @Override
+    public void deleteFeeStructure(FeeStructureDTO feeStructure) {
+
+    }
+
+    @Override
+    public List<FeeStructureDTO> getAll() {
+        return List.of();
+    }
+}
