2ae4fffc0c071444331a4ea941ab9f2f18e5bdf4
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-11 16:40:17 +0530

    feat: create service layer and controller layer

---
+++ src/main/java/edu/icet/controller/NonAcademicStaffController.java
@@ -0,0 +1,11 @@
+package edu.icet.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/non/academic/staff")
+@Tag(name = "NonAcademicStaff-Controller",description = "NonAcademicStaff API Collection...")
+public class NonAcademicStaffController {
+}
---
+++ src/main/java/edu/icet/service/impl/NonAcademicStaffServiceImpl.java
@@ -0,0 +1,30 @@
+package edu.icet.service.impl;
+
+import edu.icet.model.dto.NonAcademicStaffDTO;
+import edu.icet.service.NonAcademicStaffService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NonAcademicStaffServiceImpl implements NonAcademicStaffService {
+    @Override
+    public void addNonAcademicStaff(NonAcademicStaffDTO staff) {
+
+    }
+
+    @Override
+    public void updateNonAcademicStaff(NonAcademicStaffDTO staff) {
+
+    }
+
+    @Override
+    public void deleteNonAcademicStaff(Integer NonAcademicStaffId) {
+
+    }
+
+    @Override
+    public List<NonAcademicStaffDTO> getAll() {
+        return List.of();
+    }
+}
