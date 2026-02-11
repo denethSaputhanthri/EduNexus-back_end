54aa8a1afa2effb7018ec7cbb05424442c74d835
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-11 16:42:56 +0530

    feat: Service,Repository and Model mapper constructor inject

--- src/main/java/edu/icet/controller/NonAcademicStaffController.java
+++ src/main/java/edu/icet/controller/NonAcademicStaffController.java
@@ -1,11 +1,17 @@
 package edu.icet.controller;
 
+import edu.icet.service.NonAcademicStaffService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/non/academic/staff")
 @Tag(name = "NonAcademicStaff-Controller",description = "NonAcademicStaff API Collection...")
 public class NonAcademicStaffController {
+
+    final private NonAcademicStaffService service;
+
 }
--- src/main/java/edu/icet/service/impl/NonAcademicStaffServiceImpl.java
+++ src/main/java/edu/icet/service/impl/NonAcademicStaffServiceImpl.java
@@ -1,13 +1,21 @@
 package edu.icet.service.impl;
 
 import edu.icet.model.dto.NonAcademicStaffDTO;
+import edu.icet.repository.NonAcademicStaffRepository;
 import edu.icet.service.NonAcademicStaffService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class NonAcademicStaffServiceImpl implements NonAcademicStaffService {
+
+    final private NonAcademicStaffRepository repository;
+    final private ModelMapper mapper;
+
     @Override
     public void addNonAcademicStaff(NonAcademicStaffDTO staff) {
 
